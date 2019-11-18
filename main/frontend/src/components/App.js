7e4b73f0aba41f693b02825e1d20ef444df83250
Mikron <mikron2222@gmail.com>
2019-11-18 21:42:15 +0100

    setup react

---
+++ main/frontend/src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+
+export default App
+
+ReactDOM.render(<App />, document.getElementById('app'))
