fe30a5ca5089709f1218be5ccdb6f3fb85f626b7
Mikron <mikron2222@gmail.com>
2019-11-18 21:58:22 +0100

    exios

---
+++ main/frontend/src/components/main/List.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class List extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+
+export default List
